bef3ab0badc5fcc5de26cc2eda8c6cf1d14d9a61
akhilraj225 <40620582+akhilraj225@users.noreply.github.com>
2022-02-14 12:41:40 +0530

    Delete mysql.sh

--- mysql.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpw"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpw"
-sudo apt-get install -y mysql-server-5.7
