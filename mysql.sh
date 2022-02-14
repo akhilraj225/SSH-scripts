43457ad565d6b57acb3f8a5af9016aef31f9f251
akhil <akhil.raj@applab.qa>
2022-02-14 12:34:40 +0530

    Altered script

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,25 @@
-#!/usr/bin/env bash
-export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpw"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpw"
-sudo apt-get install -y mysql-server-5.7
+#!/bin/bash
+
+TIMESTAMP=$(date +"%F")
+BACKUP_DIR="/data/$TIMESTAMP"
+MYSQL_USER="root"
+MYSQL_PASSWORD="redhat@123"
+MYSQL="$(which mysql)"
+MYSQLDUMP="$(which mysqldump)"
+
+mkdir -p $BACKUP_DIR
+
+find "$BACKUP_DIR" -mtime +3 -type d -exec rm -rf {} \;
+
+databases=`$MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -e "show databases"| grep -Ev "(database|Database|information_schema|performance_schema|phpmyadmin|mysql)"`
+
+for db in $databases; do	        
+$MYSQLDUMP -u $MYSQL_USER -p$MYSQL_PASSWORD --databases $db > $BACKUP_DIR/$db.sql
+done
+
+if [ $? -eq 0 ]; then
+echo "Database backup successfully completed"
+else
+echo "Error found during backup"
+exit 1
+fi
